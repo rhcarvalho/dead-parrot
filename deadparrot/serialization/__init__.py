e61f14ab2331897a7d98a276e002bda678804c58
Rodolfo Carvalho <rodolfo.carvalho@corp.globo.com>
2009-06-19 17:48:06 -0300

    tell what was the unimplemented format

--- deadparrot/serialization/__init__.py
+++ deadparrot/serialization/__init__.py
@@ -28,5 +28,6 @@ class Registry(object):
         try:
             return REGISTRY_DICT[kind]
         except KeyError:
-            raise NotImplementedError, "The format %s was not implemented "
-        "as a serializer plugin for DeadParrot"
+            raise NotImplementedError, ("The format %s was not implemented as "
+                                        "a serializer plugin for DeadParrot" %
+                                        (kind,))
